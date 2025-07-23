747ba49b76b836ac5056acbe84089daf3b7fad8a
Taylan Dogan <git@taylandogan.info>
2025-07-23 02:17:58 +0200

    chore: update dotfiles
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- config-install.sh
+++ config-install.sh
@@ -6,13 +6,13 @@ TARGET_DIR=$HOME/.config
 mkdir -p $TARGET_DIR
 git init --bare $TARGET_DIR
 
-ls $WORKING_DIR/.config | xargs -I {} cp -R $WORKING_DIR/.config/{} $TARGET_DIR/{}
+ls $WORKING_DIR/config | xargs -I {} cp -R $WORKING_DIR/config/{} $TARGET_DIR/{}
 
 echo "Copied config files to $TARGET_DIR"
 
 
 # Set firefox profile
-if [[ ! -z $(which firefox)]]; then
+if [[ ! -z $(which firefox) ]]; then
     firefox -CreateProfile "home $HOME/.mozilla/firefox/profiles"
     echo "Created firefox profile 'home' in $HOME/.mozilla/firefox/profiles"
 fi
